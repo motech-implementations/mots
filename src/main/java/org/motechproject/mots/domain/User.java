e41568b3d2e681a36ff3f6010507b546b64253bb
Pawel Muchowski <pmuchowski@soldevelo.com>
2017-10-13 15:56:35 +0200

    MOTS-9: Add base entity

--- src/main/java/org/motechproject/mots/domain/User.java
+++ src/main/java/org/motechproject/mots/domain/User.java
@@ -1,27 +1,14 @@
 package org.motechproject.mots.domain;
 
-import java.util.UUID;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.Type;
 
 @Entity
 @Table
-public class User {
-
-  @Id
-  @Type(type = "uuid-char")
-  @GeneratedValue(strategy = GenerationType.AUTO)
-  @Column
-  @Getter
-  @Setter
-  private UUID id;
+public class User extends BaseEntity {
 
   @Column
   @Getter
