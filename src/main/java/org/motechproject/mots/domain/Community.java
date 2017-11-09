ddf683cc93ea3bbf647471510de29bd1772d000d
Pawel Muchowski <pmuchowski@soldevelo.com>
2017-11-09 15:38:14 +0100

    MOTS-27: Add parent references in location entities

--- src/main/java/org/motechproject/mots/domain/Community.java
+++ src/main/java/org/motechproject/mots/domain/Community.java
@@ -2,6 +2,8 @@ package org.motechproject.mots.domain;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -14,4 +16,10 @@ public class Community extends BaseEntity {
   @Getter
   @Setter
   private String name;
+
+  @ManyToOne
+  @JoinColumn(name = "facility_id")
+  @Getter
+  @Setter
+  private Facility facility;
 }
