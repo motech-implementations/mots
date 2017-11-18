2d9e35586a523679132168b8ed956480a877bb0e
Pawel Muchowski <pmuchowski@soldevelo.com>
2017-11-18 16:58:08 +0100

    MOTS-28: Create dto and mapper for AssignedModules

--- src/main/java/org/motechproject/mots/domain/IvrObject.java
+++ src/main/java/org/motechproject/mots/domain/IvrObject.java
@@ -1,11 +1,14 @@
 package org.motechproject.mots.domain;
 
+import java.util.UUID;
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @MappedSuperclass
+@NoArgsConstructor
 public abstract class IvrObject extends BaseEntity {
 
   @Column(name = "ivr_id")
@@ -17,4 +20,8 @@ public abstract class IvrObject extends BaseEntity {
   @Getter
   @Setter
   private String ivrName;
+
+  public IvrObject(UUID id) {
+    super(id);
+  }
 }
