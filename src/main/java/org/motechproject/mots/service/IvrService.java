ced519594aeb44ed85d131ef05d028ab365a9030
Pawel Muchowski <pmuchowski@soldevelo.com>
2017-11-20 13:08:24 +0100

    MOTS-28: Add subscriber to IVR group when module assigned

--- src/main/java/org/motechproject/mots/service/IvrService.java
+++ src/main/java/org/motechproject/mots/service/IvrService.java
@@ -2,7 +2,9 @@ package org.motechproject.mots.service;
 
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import org.apache.commons.lang3.StringUtils;
 import org.motechproject.mots.dto.VotoResponseDto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,6 +26,7 @@ public class IvrService {
   private static final Logger LOGGER = LoggerFactory.getLogger(IvrService.class);
   private static final String BASE_URL = "https://go.votomobile.org/api/v1";
   private static final String SUBSCRIBERS_URL = BASE_URL + "/subscribers";
+  private static final String ADD_TO_GROUPS_URL = BASE_URL + "/subscribers/groups";
 
   @Value("${mots.ivrApiKey}")
   private String ivrApiKey;
@@ -60,5 +63,30 @@ public class IvrService {
     }
 
     return null;
+  }
+
+  /**
+   * Add subscriber to IVR groups.
+   * @param subscriberId subscriber id
+   * @param groupsIds ids of groups to add subscriber
+   */
+  public void addSubscriberToGroups(String subscriberId, List<String> groupsIds) {
+    Map<String, String> params = new HashMap<>();
+    params.put("api_key", ivrApiKey);
+    params.put("subscriber_ids", subscriberId);
+    params.put("groups", StringUtils.join(groupsIds, ","));
+
+    HttpHeaders headers = new HttpHeaders();
+    headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+
+    HttpEntity<Map<String, String>> request = new HttpEntity<>(params, headers);
+
+    try {
+      restTemplate.exchange(ADD_TO_GROUPS_URL, HttpMethod.POST, request, String.class);
+    } catch (HttpClientErrorException ex) {
+      LOGGER.error(ex.getResponseBodyAsString(), ex);
+    } catch (Exception ex) {
+      LOGGER.error(ex.getMessage(), ex);
+    }
   }
 }
