42da831456827c1b77f759db3305483a5e66b935
Pawel Muchowski <pmuchowski@soldevelo.com>
2017-11-20 16:12:24 +0100

    MOTS-28: Create endpoint to get modules assigned to CHW

--- src/main/java/org/motechproject/mots/web/ModuleAssignmentController.java
+++ src/main/java/org/motechproject/mots/web/ModuleAssignmentController.java
@@ -1,7 +1,9 @@
 package org.motechproject.mots.web;
 
+import java.util.UUID;
 import org.motechproject.mots.domain.AssignedModules;
-import org.motechproject.mots.dto.AssignedModulesDto;
+import org.motechproject.mots.dto.ChwModulesDto;
+import org.motechproject.mots.dto.ModuleAssignmentDto;
 import org.motechproject.mots.mapper.ModuleAssignmentMapper;
 import org.motechproject.mots.service.ModuleAssignmentService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
@@ -22,13 +26,28 @@ public class ModuleAssignmentController extends BaseController {
 
   /**
    * Assign modules for CHW.
-   * @param assignedModulesDto dto with chw id and list of modules assigned to it
+   * @param moduleAssignmentDto dto with chw id and list of modules assigned to it
    */
   @RequestMapping(value = "/assignModules", method = RequestMethod.POST)
   @ResponseStatus(HttpStatus.OK)
-  public void assignModules(@RequestBody AssignedModulesDto assignedModulesDto) {
-    AssignedModules assignedModules = moduleAssignmentMapper.fromDto(assignedModulesDto);
+  public void assignModules(@RequestBody ModuleAssignmentDto moduleAssignmentDto) {
+    AssignedModules assignedModules = moduleAssignmentMapper.fromDto(moduleAssignmentDto);
 
     moduleAssignmentService.assignModules(assignedModules);
+  }
+
+  /**
+   * Get Modules assigned to CHW.
+   * @param chwId id of CHW
+   * @return modules assigned to CHW
+   */
+  @RequestMapping(value = "/assignedModules", method = RequestMethod.GET)
+  @ResponseStatus(HttpStatus.OK)
+  @ResponseBody
+  public ChwModulesDto getAssignedModules(@RequestParam(value = "chwId") String chwId) {
+    AssignedModules assignedModules =
+        moduleAssignmentService.getAssignedModules(UUID.fromString(chwId));
+
+    return moduleAssignmentMapper.toDto(assignedModules);
   }
 }
