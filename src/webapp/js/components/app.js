bddbd9f9f724f5a92574296385c933448c024caa
dpiasecki <dpiasecki@soldevelo.com>
2018-01-09 15:04:48 +0100

    Menu icons fixes, container max width

--- src/webapp/js/components/app.js
+++ src/webapp/js/components/app.js
@@ -47,17 +47,19 @@ export default class App extends Component {
           hideMenuSmart={this.hideMenuSmart}
         />
         <div id="page-wrapper" className={this.state.showMenuSmart ? 'hide-max-r-xsmall-max' : ''}>
-          <div className="container-fluid">
-            <Switch>
-              <Route path="/modules/assign/:chwId?" component={AssignModules} />
-              <Route path="/incharge/new" component={InchargeNew} />
-              <Route path="/incharge/:inchargeId" component={InchargeEdit} />
-              <Route path="/incharge" component={Incharges} />
-              <Route path="/chw/new" component={HealthWorkersNew} />
-              <Route path="/chw/:chwId" component={HealthWorkersEdit} />
-              <Route path="/chw" component={HealthWorkers} />
-              <Route path="/" component={Home} />
-            </Switch>
+          <div className="container-wrapper">
+            <div className="container-fluid">
+              <Switch>
+                <Route path="/modules/assign/:chwId?" component={AssignModules} />
+                <Route path="/incharge/new" component={InchargeNew} />
+                <Route path="/incharge/:inchargeId" component={InchargeEdit} />
+                <Route path="/incharge" component={Incharges} />
+                <Route path="/chw/new" component={HealthWorkersNew} />
+                <Route path="/chw/:chwId" component={HealthWorkersEdit} />
+                <Route path="/chw" component={HealthWorkers} />
+                <Route path="/" component={Home} />
+              </Switch>
+            </div>
           </div>
         </div>
 
