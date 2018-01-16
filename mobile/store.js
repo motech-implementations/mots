e1829caa9be2bd8ab7df6fc6f99e4ab6dcd8fd42
dpiasecki <dpiasecki@soldevelo.com>
2018-01-16 15:40:26 +0100

    Redux-offline

---
+++ mobile/store.js
@@ -0,0 +1,17 @@
+import ReduxPromise from 'redux-promise';
+import reduxThunk from 'redux-thunk';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { offline } from 'redux-offline';
+import offlineConfig from 'redux-offline/lib/defaults';
+
+import reducers from '../src/webapp/js/reducers/index';
+
+const store = createStore(
+  reducers,
+  compose(
+    applyMiddleware(reduxThunk, ReduxPromise),
+    offline(offlineConfig),
+  ),
+);
+
+export default store;
