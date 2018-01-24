6a8718d73598d77f8072ea45b59d5a3b99e19d59
dpiasecki <dpiasecki@soldevelo.com>
2018-01-24 13:33:52 +0100

    Redux persist to save redux in AsyncStorage

--- mobile/store.js
+++ mobile/store.js
@@ -1,13 +1,22 @@
 import ReduxPromise from 'redux-promise';
 import reduxThunk from 'redux-thunk';
 import { createStore, applyMiddleware, compose } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import { offline } from 'redux-offline';
 import offlineConfig from 'redux-offline/lib/defaults';
 
 import reducers from './reducers';
 
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
 const store = createStore(
-  reducers,
+  persistedReducer,
   compose(
     applyMiddleware(reduxThunk, ReduxPromise),
     offline(offlineConfig),
