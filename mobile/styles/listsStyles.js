d86e9f9e6ad91b694b2d268141e88c23f1909ee6
dpiasecki <dpiasecki@soldevelo.com>
2018-01-24 15:18:01 +0100

    Add community health workers list

---
+++ mobile/styles/listsStyles.js
@@ -0,0 +1,54 @@
+export default {
+  container: {
+    marginTop: 60,
+    marginBottom: 20,
+    backgroundColor: '#FFF',
+  },
+  title: {
+    fontSize: 36,
+    marginHorizontal: 40,
+    marginVertical: 10,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+  },
+  actionButton: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 60,
+    borderRadius: 3,
+    backgroundColor: '#337ab7',
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+  },
+  actionButtonAssign: {
+    backgroundColor: '#449C44',
+    marginLeft: 5,
+    width: 150,
+  },
+  buttonLabel: {
+    marginLeft: 5,
+    color: '#FFF',
+  },
+  card: {
+    borderColor: '#ebebeb',
+    borderRadius: 3,
+    borderWidth: 1,
+    marginHorizontal: 40,
+    marginBottom: 20,
+    padding: 10,
+    backgroundColor: '#f8f8f8',
+    justifyContent: 'center',
+  },
+  cardLine: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  bold: {
+    fontWeight: 'bold',
+  },
+  normal: {
+    fontWeight: 'normal',
+  },
+};
