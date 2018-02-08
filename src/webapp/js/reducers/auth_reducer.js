18dedcb9b0aae572ebbd9116950d3cf17581d8e5
Pawel Muchowski <pmuchowski@soldevelo.com>
2018-02-08 18:31:03 +0100

    MOTS-192: Reset session countdown on module tree actions

--- src/webapp/js/reducers/auth_reducer.js
+++ src/webapp/js/reducers/auth_reducer.js
@@ -1,4 +1,4 @@
-import { AUTH_USER, UNAUTH_USER, AUTH_ERROR, SET_COUNTER_LOGOUT_TIME } from '../actions/types';
+import { AUTH_USER, UNAUTH_USER, AUTH_ERROR, SET_COUNTER_LOGOUT_TIME, RESET_LOGOUT_COUNTER } from '../actions/types';
 
 const initialState = {
   error: null,
@@ -17,6 +17,8 @@ export default function (state = initialState, action) {
       return { ...state, authenticated: false };
     case SET_COUNTER_LOGOUT_TIME:
       return { ...state, counterLogoutTime: action.payload, resetCounter: !state.resetCounter };
+    case RESET_LOGOUT_COUNTER:
+      return { ...state, resetCounter: !state.resetCounter };
     case AUTH_ERROR:
       return { ...state, error: action.payload, authenticated: false };
     default:
