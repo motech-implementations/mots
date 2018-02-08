1b2aca880dc33160424d07c8518db472480370d4
Artur Walkowiak <awalkowiak@soldevelo.com>
2018-02-08 16:47:17 +0100

    CHW LIst on smaller resolutions fix

--- mobile/components/HealthWorkers.js
+++ mobile/components/HealthWorkers.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, PixelRatio } from 'react-native';
 import HealthWorkersList from '../container/HealthWorkersList';
 
 import styles from '../styles/listsStyles';
 
 const Incharges = () => (
   <View style={styles.container}>
-    <Text style={styles.title}>Community Health Workers</Text>
+    <Text style={styles.title}>
+      {PixelRatio.get() < 2 ? 'CHW List' : 'Community Health Workers'}
+    </Text>
     <HealthWorkersList />
   </View>
 );
