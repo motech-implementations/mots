dc55566e2f69cc8b13540a57206691739c7034a7
Daniel Serkowski <dserkowski@soldevelo.com>
2018-02-09 21:32:39 +0100

    MOTS-151: Added fields s' constraints in CommunityHealthWorkerDto

--- src/main/java/org/motechproject/mots/dto/CommunityHealthWorkerDto.java
+++ src/main/java/org/motechproject/mots/dto/CommunityHealthWorkerDto.java
@@ -2,25 +2,37 @@ package org.motechproject.mots.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.constraints.NotEmpty;
+import org.motechproject.mots.constants.ValidationMessages;
 import org.motechproject.mots.validate.annotations.DateOfBirth;
+import org.motechproject.mots.validate.annotations.EducationLevel;
+import org.motechproject.mots.validate.annotations.Gender;
+import org.motechproject.mots.validate.annotations.Language;
+import org.motechproject.mots.validate.annotations.Literacy;
 import org.motechproject.mots.validate.annotations.PhoneNumber;
+import org.motechproject.mots.validate.annotations.Uuid;
 
 public class CommunityHealthWorkerDto {
 
   @Getter
   @Setter
+  @Uuid(message = ValidationMessages.INVALID_ID)
   private String id;
 
   @Getter
   @Setter
+  @NotBlank(message = ValidationMessages.EMPTY_CHW_ID)
   private String chwId;
 
   @Getter
   @Setter
+  @NotBlank(message = ValidationMessages.EMPTY_FIRST_NAME)
   private String firstName;
 
   @Getter
   @Setter
+  @NotBlank(message = ValidationMessages.EMPTY_SECOND_NAME)
   private String secondName;
 
   @Getter
@@ -30,18 +42,23 @@ public class CommunityHealthWorkerDto {
   @Getter
   @Setter
   @DateOfBirth
+  @NotEmpty(message = ValidationMessages.EMPTY_DATE_OF_BIRTH)
   private String dateOfBirth;
 
   @Getter
   @Setter
+  @Gender
+  @NotEmpty(message = ValidationMessages.EMPTY_GENDER)
   private String gender;
 
   @Getter
   @Setter
+  @Literacy
   private String literacy;
 
   @Getter
   @Setter
+  @EducationLevel
   private String educationLevel;
 
   @Getter
@@ -79,6 +96,8 @@ public class CommunityHealthWorkerDto {
 
   @Getter
   @Setter
+  @Uuid(message = ValidationMessages.INVALID_COMMUNITY_ID)
+  @NotEmpty(message = ValidationMessages.EMPTY_COMMUNITY)
   private String communityId;
 
   @Getter
@@ -91,5 +110,7 @@ public class CommunityHealthWorkerDto {
 
   @Getter
   @Setter
+  @Language
+  @NotEmpty(message = ValidationMessages.EMPTY_PREFERRED_LANGUAGE)
   private String preferredLanguage;
 }
---
+++ src/main/java/org/motechproject/mots/validate/annotations/Literacy.java
@@ -0,0 +1,39 @@
+package org.motechproject.mots.validate.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import org.motechproject.mots.constants.ValidationMessages;
+import org.motechproject.mots.validate.constraintvalidators.LiteracyValidator;
+
+@Target(ElementType.FIELD)
+@Constraint(validatedBy = {LiteracyValidator.class})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Literacy {
+
+  /**
+   * Specify the message in case of a validation error
+   *
+   * @return the message about the error
+   */
+  String message() default ValidationMessages.INVALID_LITERACY;
+
+  /**
+   * Specify validation groups, to which this constraint belongs
+   *
+   * @return array with group classes
+   */
+  Class<?>[] groups() default {
+  };
+
+  /**
+   * Specify custom payload objects
+   *
+   * @return array with payload classes
+   */
+  Class<? extends Payload>[] payload() default {
+  };
+}
