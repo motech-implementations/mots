36ee05883a6252e3c1c383e023835010517204ca
Karol Cissewski <kcissewski@soldevelo.com>
2018-02-12 10:32:12 +0100

    MOTS-114: Remove unnecessary code from LocationMapper

--- src/main/java/org/motechproject/mots/mapper/LocationMapper.java
+++ src/main/java/org/motechproject/mots/mapper/LocationMapper.java
@@ -1,21 +1,13 @@
 package org.motechproject.mots.mapper;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.UUID;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
 import org.mapstruct.ReportingPolicy;
 import org.mapstruct.factory.Mappers;
-import org.motechproject.mots.domain.Community;
 import org.motechproject.mots.domain.District;
 import org.motechproject.mots.domain.Facility;
-import org.motechproject.mots.dto.CommunityDto;
 import org.motechproject.mots.dto.DistrictDto;
 import org.motechproject.mots.dto.FacilityDto;
 
@@ -30,30 +22,8 @@ public interface LocationMapper {
    * @param districts list of districts
    * @return list of district dtos
    */
-  default List<DistrictDto> toDistrictDtos(List<District> districts) {
-    return toDistrictDto(districts);
-  }
-
-  List<DistrictDto> toDistrictDto(List<District> list);
+  List<DistrictDto> toDistrictDtos(List<District> districts);
 
   @Mappings({@Mapping(target = "inchargeId", source = "incharge.id")})
   FacilityDto toDto(Facility facility);
-
-
-  /**
-   * Convert community list to community dtos map.
-   * @param communities list of communities
-   * @return map of community dtos, where the key is the community id
-   */
-  default Map<String, CommunityDto> toCommunityDtos(Set<Community> communities) {
-    Map<UUID, Community> map = new HashMap<>();
-
-    if (communities != null) {
-      map = communities.stream().collect(Collectors.toMap(Community::getId, Function.identity()));
-    }
-
-    return toCommunityDto(map);
-  }
-
-  Map<String, CommunityDto> toCommunityDto(Map<UUID, Community> map);
 }
