c63e7519eb46e1b99e7af7fd6d7641f36c59954b
Karol Cissewski <kcissewski@soldevelo.com>
2018-02-14 14:52:40 +0100

    MOTS-114: Fix Locations Reducers

--- mobile/reducers/locations_reducer.js
+++ mobile/reducers/locations_reducer.js
@@ -1,9 +1,9 @@
 import { FETCH_LOCATIONS } from '../actions/types';
 
-export default function (state = {}, action) {
+export default function (state = [], action) {
   switch (action.type) {
     case FETCH_LOCATIONS:
-      return action.payload || {};
+      return action.payload || [];
     default:
       return state;
   }
--- src/webapp/js/reducers/locations_reducer.js
+++ src/webapp/js/reducers/locations_reducer.js
@@ -1,6 +1,6 @@
 import { FETCH_LOCATIONS } from '../actions/types';
 
-export default function (state = {}, action) {
+export default function (state = [], action) {
   switch (action.type) {
     case FETCH_LOCATIONS:
       if (action.payload.data !== undefined) {
