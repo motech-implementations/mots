9a3c93d3516fd4cbeedfd17e5e5ed87fc3ed36dd
Daniel Serkowski <dserkowski@soldevelo.com>
2018-02-15 17:22:24 +0100

    MOTS-60: Added test IVR config

---
+++ src/main/resources/test_ivr_config.sql
@@ -0,0 +1,8 @@
+INSERT INTO	`ivr_config` (`id`, `create_date`, `update_date`, `base_url`, `default_users_group_id`, `detect_voicemail_action`, `module_assigned_message_id`, `retry_attempts_long`, `retry_attempts_short`, `retry_delay_long`, `retry_delay_short`, `send_sms_if_voice_fails`) VALUES	(		'70638cc4-4c33-4899-8480-a4ef1607273b', '2018-02-15 16:07:22.745000', '2018-02-15 16:07:22.745000', 'https://go.votomobile.org/api/v1', '298868', true, '2228099', 1, 3, 60, 15, true	);
+
+INSERT INTO `mots`.`ivr_config_languages` (`ivr_config_id`, `ivr_language_id`, `language`) VALUES ('70638cc4-4c33-4899-8480-a4ef1607273b', '206945', 'ENGLISH');
+INSERT INTO `mots`.`ivr_config_languages` (`ivr_config_id`, `ivr_language_id`, `language`) VALUES ('70638cc4-4c33-4899-8480-a4ef1607273b', '207107', 'KRIO');
+INSERT INTO `mots`.`ivr_config_languages` (`ivr_config_id`, `ivr_language_id`, `language`) VALUES ('70638cc4-4c33-4899-8480-a4ef1607273b', '207108', 'LIMBA');
+INSERT INTO `mots`.`ivr_config_languages` (`ivr_config_id`, `ivr_language_id`, `language`) VALUES ('70638cc4-4c33-4899-8480-a4ef1607273b', '207109', 'SUSU');
+INSERT INTO `mots`.`ivr_config_languages` (`ivr_config_id`, `ivr_language_id`, `language`) VALUES ('70638cc4-4c33-4899-8480-a4ef1607273b', '207110', 'TEMNE');
+INSERT INTO `mots`.`ivr_config_languages` (`ivr_config_id`, `ivr_language_id`, `language`) VALUES ('70638cc4-4c33-4899-8480-a4ef1607273b', '207111', 'MENDE');
