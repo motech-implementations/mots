36e268bf0740c04c32851af5f95320026860a9fd
kcissewski <kcissewski@soldevelo.com>
2018-02-21 16:44:22 +0100

    Remove unused import

--- src/main/java/org/motechproject/mots/web/UserController.java
+++ src/main/java/org/motechproject/mots/web/UserController.java
@@ -12,7 +12,6 @@ import org.motechproject.mots.mapper.UserMapper;
 import org.motechproject.mots.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
