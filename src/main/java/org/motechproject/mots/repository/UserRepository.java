aacf4889fe8459255e7adfe5a4f54ebcd64ac08a
przemyslawStudzinskiSolDevelo <pstudzinski@soldevelo.com>
2018-02-26 12:11:36 +0100

    MOTS-302: Chenged exted from Crud to Jpa in User Repository

--- src/main/java/org/motechproject/mots/repository/UserRepository.java
+++ src/main/java/org/motechproject/mots/repository/UserRepository.java
@@ -4,9 +4,9 @@ import java.util.Optional;
 import java.util.UUID;
 import org.motechproject.mots.domain.security.User;
 import org.motechproject.mots.repository.custom.UserRepositoryCustom;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends CrudRepository<User, UUID>, UserRepositoryCustom {
+public interface UserRepository extends JpaRepository<User, UUID>, UserRepositoryCustom {
 
   Optional<User> findById(UUID id);
 
