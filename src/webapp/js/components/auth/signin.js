8e8f4441d7b6c840e759866f67acbd50c4ff97e6
dhatala <dhatala@soldevelo.com>
2018-07-12 17:20:42 +0200

    MOTS-558 new logo and footer added

--- src/webapp/js/components/auth/signin.js
+++ src/webapp/js/components/auth/signin.js
@@ -27,19 +27,19 @@ class Signin extends Component {
     const resultingClassName = `input-group margin-bottom-md ${touched && error ? 'has-error' : ''}`;
 
     return (
-        <div className={resultingClassName}>
-          <span className="input-group-addon"><i className={`glyphicon glyphicon-${icon}`} /></span>
-          <input className="form-control" type={type} placeholder={placeholder} {...input} />
-        </div>
+      <div className={resultingClassName}>
+        <span className="input-group-addon"><i className={`glyphicon glyphicon-${icon}`} /></span>
+        <input className="form-control" type={type} placeholder={placeholder} {...input} />
+      </div>
     );
   }
 
   renderAlert() {
     if (this.props.errorMessage) {
       return (
-          <div id="login-alert" className="alert alert-danger col-sm-12">
-            <strong>{this.props.errorMessage}</strong>
-          </div>
+        <div id="login-alert" className="alert alert-danger col-sm-12">
+          <strong>{this.props.errorMessage}</strong>
+        </div>
       );
     }
 
@@ -50,57 +50,57 @@ class Signin extends Component {
     const { handleSubmit } = this.props;
 
     return (
-        <div>
-          <div className="mainbox col-md-6 col-md-offset-3
-        col-sm-8 col-sm-offset-2"
-          >
-            <div className="row">
-              <div className="col-md-8 col-md-offset-2">
-                <img className="img-responsive" alt="test" src="/EBODAClogo-RGB-with.jpg" />
-              </div>
+      <div>
+        <div className="mainbox col-md-6 col-md-offset-3
+      col-sm-8 col-sm-offset-2"
+        >
+          <div className="row">
+            <div className="col-md-8 col-md-offset-2">
+              <img className="img-responsive" alt="test" src="/EBODAClogo-RGB-with.jpg" />
             </div>
+          </div>
 
-            <div className="row">
-              <div className="col-md-8 col-md-offset-2">
-                <div className="panel panel-info">
-                  <div className="panel-heading">
-                    <div className="panel-title">Sign In</div>
-                  </div>
-                  <div className="panel-body padding-top-lg">
-                    { this.renderAlert() }
+          <div className="row">
+            <div className="col-md-8 col-md-offset-2">
+              <div className="panel panel-info">
+                <div className="panel-heading">
+                  <div className="panel-title">Sign In</div>
+                </div>
+                <div className="panel-body padding-top-lg">
+                  { this.renderAlert() }
 
-                    <form className="form-horizontal" onSubmit={handleSubmit(this.onSubmit)}>
-                      <Field
-                          placeholder="Username"
-                          type="text"
-                          name="username"
-                          icon="user"
-                          component={Signin.renderField}
-                          className="form-control"
-                      />
-                      <Field
-                          placeholder="Password"
-                          type="password"
-                          name="password"
-                          icon="lock"
-                          component={Signin.renderField}
-                          className="form-control"
-                      />
-                      <div className="form-group">
-                        <div className="col-sm-12 controls">
-                          <button type="submit" className="btn btn-success">Login</button>
-                        </div>
+                  <form className="form-horizontal" onSubmit={handleSubmit(this.onSubmit)}>
+                    <Field
+                      placeholder="Username"
+                      type="text"
+                      name="username"
+                      icon="user"
+                      component={Signin.renderField}
+                      className="form-control"
+                    />
+                    <Field
+                      placeholder="Password"
+                      type="password"
+                      name="password"
+                      icon="lock"
+                      component={Signin.renderField}
+                      className="form-control"
+                    />
+                    <div className="form-group">
+                      <div className="col-sm-12 controls">
+                        <button type="submit" className="btn btn-success">Login</button>
                       </div>
-                    </form>
-                  </div>
+                    </div>
+                  </form>
                 </div>
               </div>
             </div>
           </div>
-          <div>
-            <FooterInfo />
-          </div>
         </div>
+        <div>
+          <FooterInfo />
+        </div>
+      </div>
     );
   }
 }
