b416d2fc8172526ec3a1137b808cf8f3e007426f
Pawel Muchowski <pmuchowski@soldevelo.com>
2018-10-26 17:17:53 +0200

    MOTS-577: Add type field to Choice and remove 'isCorrect' field

--- src/main/java/org/motechproject/mots/domain/Choice.java
+++ src/main/java/org/motechproject/mots/domain/Choice.java
@@ -2,11 +2,14 @@ package org.motechproject.mots.domain;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.Type;
+import org.motechproject.mots.domain.enums.ChoiceType;
 
 @Entity
 @Table(name = "choice")
@@ -23,10 +26,11 @@ public class Choice extends BaseTimestampedEntity {
   @Setter
   private String ivrName;
 
-  @Column(name = "is_correct", nullable = false)
+  @Enumerated(EnumType.STRING)
+  @Column(name = "type", nullable = false)
   @Getter
   @Setter
-  private Boolean isCorrect;
+  private ChoiceType type;
 
   @Type(type = "text")
   @Column(name = "description")
@@ -34,14 +38,14 @@ public class Choice extends BaseTimestampedEntity {
   @Setter
   private String description;
 
-  private Choice(Integer choiceId, String ivrName, Boolean isCorrect, String description) {
+  private Choice(Integer choiceId, String ivrName, ChoiceType type, String description) {
     this.choiceId = choiceId;
     this.ivrName = ivrName;
-    this.isCorrect = isCorrect;
+    this.type = type;
     this.description = description;
   }
 
   public Choice copyAsNewDraft() {
-    return new Choice(choiceId, ivrName, isCorrect, description);
+    return new Choice(choiceId, ivrName, type, description);
   }
 }
---
+++ src/main/java/org/motechproject/mots/domain/enums/ChoiceType.java
@@ -0,0 +1,30 @@
+package org.motechproject.mots.domain.enums;
+
+import lombok.Getter;
+
+public enum ChoiceType {
+  CORRECT("Correct"),
+  INCORRECT("Incorrect"),
+  DONT_KNOW("I don't know");
+
+  @Getter
+  private String displayName;
+
+  ChoiceType(String displayName) {
+    this.displayName = displayName;
+  }
+
+  /**
+   * Return an enum value from display name.
+   * @param name display name of enum value
+   */
+  public static ChoiceType getByDisplayName(String name) {
+    for (ChoiceType choiceType : ChoiceType.values()) {
+      if (choiceType.getDisplayName().equalsIgnoreCase(name)) {
+        return choiceType;
+      }
+    }
+
+    return null;
+  }
+}
