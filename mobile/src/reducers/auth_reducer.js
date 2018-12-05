5fc9ac90a47924ee8945e23513a2b902b83f1d40
Jakub Kondrat <jkondrat@soldevelo.com>
2018-12-05 14:44:30 +0100

    MOTS-645: Fix login issue after restoring app state on Android 4.1 & 4.2

--- mobile/src/reducers/auth_reducer.js
+++ mobile/src/reducers/auth_reducer.js
@@ -2,37 +2,49 @@ import { AsyncStorage } from 'react-native';
 
 import { AUTH_USER, UNAUTH_USER, AUTH_ERROR, STORE_LOGIN } from '../actions/types';
 
-const initialState = {
-  error: null,
+const CLEAR_LOGIN_STATE = {
   authenticated: false,
   expirationTime: null,
+  accessToken: null,
+  refreshToken: null,
+};
+
+const initialState = {
+  error: null,
   savedLogins: {},
+  ...CLEAR_LOGIN_STATE,
 };
 
 const OFFLINE_LOGIN_DURATION = 1800;
 
-function clearLoginState() {
+function clearTokens() {
   AsyncStorage.removeItem('token');
   AsyncStorage.removeItem('refresh_token');
-  return { authenticated: false, expirationTime: null };
 }
 
 export default function (state = initialState, action) {
   switch (action.type) {
     case AUTH_USER: {
-      const expirationTime = (action.payload) ?
+      const { isOffline, accessToken, refreshToken } = action.payload;
+      AsyncStorage.setItem('token', accessToken);
+      AsyncStorage.setItem('refresh_token', refreshToken);
+      const expirationTime = (isOffline) ?
         new Date().getTime() + (OFFLINE_LOGIN_DURATION * 1000) : null;
       return {
         ...state,
         error: null,
         authenticated: true,
         expirationTime,
+        accessToken,
+        refreshToken,
       };
     }
     case UNAUTH_USER:
-      return { ...state, ...clearLoginState() };
+      clearTokens();
+      return { ...state, ...CLEAR_LOGIN_STATE };
     case AUTH_ERROR:
-      return { ...state, ...clearLoginState(), error: action.payload };
+      clearTokens();
+      return { ...state, ...CLEAR_LOGIN_STATE, error: action.payload };
     case STORE_LOGIN: {
       const { username, hash, token } = action.payload;
       return {
