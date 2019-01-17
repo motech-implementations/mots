34939adb98510965772b3be407d464f9ff2ba303
Kristof Kaczmarczyk <kkaczmarczyk@soldevelo.com>
2019-01-17 17:23:10 +0100

    set explicit paramter options for Facilities Report

---
+++ src/main/resources/db/changelog/20190117_1716.mysql.sql
@@ -0,0 +1,14 @@
+--liquibase formatted sql
+
+--changeset kkaczmarczyk:1547741817171-1
+UPDATE mots.jasper_template_parameter_options
+SET 
+WHERE id='8c50ed66-d753-445c-b535-b39f36ce433e' AND `options`='CHC:CHC';
+
+UPDATE mots.jasper_template_parameter_options
+SET 
+WHERE id='8c50ed66-d753-445c-b535-b39f36ce433e' AND `options`='CHP:CHP';
+
+UPDATE mots.jasper_template_parameter_options
+SET 
+WHERE id='8c50ed66-d753-445c-b535-b39f36ce433e' AND `options`='MCHP:MCHP';
