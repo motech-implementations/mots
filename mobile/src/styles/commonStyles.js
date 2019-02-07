7abb6274f7dc49c4ae815adf9321da317689fb87
Jakub Kondrat <jkondrat@soldevelo.com>
2019-02-07 17:28:42 +0100

    MOTS-705: Fix header overlapping title

--- mobile/src/styles/commonStyles.js
+++ mobile/src/styles/commonStyles.js
@@ -1,11 +1,15 @@
+import { getStatusBarHeight } from 'react-native-status-bar-height';
+
+const statusBarHeight = getStatusBarHeight(true);
+
 export default {
   container: {
     flex: 1,
-    marginTop: 40,
+    marginTop: 40 + statusBarHeight,
     backgroundColor: '#FFF',
   },
   containerHighResolutions: {
-    marginTop: 60,
+    marginTop: 60 + statusBarHeight,
   },
   center: {
     flex: 1,
