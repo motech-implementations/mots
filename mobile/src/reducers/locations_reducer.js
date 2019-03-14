447bdd95f318f0e27db2a5c41398cf00b4ba0aab
Jakub Kondrat <jkondrat@soldevelo.com>
2019-03-14 14:58:08 +0100

    WIP

--- mobile/src/reducers/locations_reducer.js
+++ mobile/src/reducers/locations_reducer.js
@@ -3,6 +3,7 @@ import { FETCH_LOCATIONS } from '../actions/types';
 export default function (state = [], action) {
   switch (action.type) {
     case FETCH_LOCATIONS:
+      console.log('loc payload', action.payload);
       if (action.payload !== null) {
         return action.payload || [];
       }
