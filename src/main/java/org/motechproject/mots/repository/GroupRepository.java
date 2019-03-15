5eefaaadb2131078bd80a405f9c59ecc749b733c
Jakub Kondrat <jkondrat@soldevelo.com>
2019-03-15 12:20:13 +0100

    MOTS-772: Sort groups alphabetically

--- src/main/java/org/motechproject/mots/repository/GroupRepository.java
+++ src/main/java/org/motechproject/mots/repository/GroupRepository.java
@@ -1,11 +1,13 @@
 package org.motechproject.mots.repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import org.motechproject.mots.domain.Group;
 import org.springframework.data.repository.CrudRepository;
 
 public interface GroupRepository extends CrudRepository<Group, UUID> {
+  List<Group> findAllByOrderByNameAsc();
 
   Optional<Group> findById(UUID id);
 
